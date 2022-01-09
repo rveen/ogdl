954fe884464b091a6439219981958111f928faa7
rveen <rolf.veen@gmail.com>
2022-01-09 11:35:10 +0100

    SortAsString, SortAsInt

--- sort.go
+++ sort.go
@@ -10,14 +10,9 @@ type stringSorter struct {
 	array  []*Graph
 }
 
-func (g *Graph) SortByString(fields string) {
-
-	ff := strings.Fields(fields)
-
-	for _, f := range ff {
-		s := stringSorter{f, g.Out}
-		sort.Sort(s)
-	}
+func (g *Graph) SortAsString(field string) {
+	s := stringSorter{field, g.Out}
+	sort.Sort(s)
 }
 
 func (g stringSorter) Swap(i, j int) { g.array[i], g.array[j] = g.array[j], g.array[i] }
@@ -29,5 +24,27 @@ func (g stringSorter) Less(i, j int) bool {
 	return strings.Compare(a, b) == -1
 }
 func (g stringSorter) Len() int {
+	return len(g.array)
+}
+
+type intSorter struct {
+	fields string
+	array  []*Graph
+}
+
+func (g *Graph) SortAsInt(field string) {
+	s := intSorter{field, g.Out}
+	sort.Sort(s)
+}
+
+func (g intSorter) Swap(i, j int) { g.array[i], g.array[j] = g.array[j], g.array[i] }
+func (g intSorter) Less(i, j int) bool {
+
+	a := g.array[i].Get(g.fields).Int64()
+	b := g.array[j].Get(g.fields).Int64()
+
+	return a < b
+}
+func (g intSorter) Len() int {
 	return len(g.array)
 }
